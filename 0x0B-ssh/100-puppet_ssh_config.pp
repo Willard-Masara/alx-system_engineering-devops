2082c6bde2e214d73de7238650c2235196b67e68
Willard-Masara <willardmasarat@gmail.com>
2023-11-27 11:22:19 -0800

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# this script makes changes to our config file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
---
+++ 0x0B-ssh/README.md
@@ -0,0 +1,1 @@
+This is the readme for the SSH project. We are using ssh to connect to our serveres and to connect others to ur server
