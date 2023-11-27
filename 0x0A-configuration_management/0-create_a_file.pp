6bbd087657f8e53bfbafbaf60e80fad062a94436
Willard-Masara <willardmasarat@gmail.com>
2023-11-27 02:24:34 -0800

    puppert master

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#THe manifest ensures there is a file with priviledges
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
