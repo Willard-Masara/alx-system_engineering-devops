acf6e68a412475e1589c18f749f3b871d6982d7c
Willard-Masara <willardmasarat@gmail.com>
2024-01-19 22:19:25 -0800

    fix the 500 status code

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,21 @@
 # automating a 500 status return
 
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+# Replace line containing "phpp" with "php"
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin'],
+}
+
+# Manage Apache2 configuration file
 file { '/etc/apache2/apache2.conf':
   ensure => file,
   source => 'puppet:///modules/my_module/apache2.conf',
   notify => Service['apache2'],
 }
 
+# Manage Apache2 service
 service { 'apache2':
   ensure    => running,
   enable    => true,
